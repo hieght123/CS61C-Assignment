7d7e10c0d6fb759038dee1e88f05c7b766c662b2
wang <height5987@gmail.com>
2025-10-21 14:57:00 +0800

    lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    node * turtle = head;
+    node * rabbit = head;
+    if(head == NULL)return 0;
+
+    do{
+        for(int i =0;i<2;i++){
+            if(rabbit->next == NULL){
+                return 0;
+            }
+            rabbit = rabbit->next;
+        }
+        turtle = turtle->next;
+    }while( turtle != rabbit);
+    return 1;
 }
